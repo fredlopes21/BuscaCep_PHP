dc68c6fc1a3fa9cce9096e01414e37a74ff76251
mari0611 <maariana.reis@hotmail.com>
2019-12-10 08:44:11 -0300

    Busca inicial no ViaCep

---
+++ src/Search.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace maari\buscaCep;
+
+class Search{
+    private $url = "https://viacep.com.br/ws/";
+
+    public function getAddressFromZipcode(string $zipcode): array {
+        $zipcode = preg_replace('/[^0-9]/im', '', $zipcode);
+
+        $get = file_get_contents($this->url . $zipcode . "/json");
+
+        return(array) json_decode($get);
+    }
+}
